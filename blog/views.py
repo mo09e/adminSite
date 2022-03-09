18fed829b23322d03c741351017da7b7d9942b8f
Moe <moe_akahane@diveintocode.jp>
2022-03-09 15:15:56 +0900

    First Commit

---
+++ blog/views.py
@@ -0,0 +1,67 @@
+from django.shortcuts import render
+from django.shortcuts import redirect
+from .models import Article
+from .forms import ArticleForm
+from django.contrib.auth.decorators import login_required # --- 追記
+
+
+@login_required
+def index(request):
+    articles = Article.objects.all()
+    params = {
+        'articles': articles,
+    }
+    return render(request, 'blog/index.html', params)
+
+
+@login_required
+def create(request):
+    if (request.method == 'POST'):
+        obj = Article()
+        article = ArticleForm(request.POST, instance=obj)
+        article.save()
+        return redirect('index')
+    else:
+        params = {
+            'form': ArticleForm(),
+        }
+        return render(request, 'blog/create.html', params)
+
+
+def detail(request, article_id):
+    article = Article.objects.get(id=article_id)
+    params = {
+        'article': article,
+    }
+    return render(request, 'blog/detail.html', params)
+
+
+def edit(request, article_id):
+    article = Article.objects.get(id=article_id)
+    if (request.method == 'POST'):
+        article.title = request.POST['title']
+        article.content = request.POST['content']
+        article.save()
+        return redirect('detail', article_id)
+    else:
+        form = ArticleForm(initial={
+            'title': article.title,
+            'content': article.content,
+            })
+        params = {
+            'article': article,
+            'form': form,
+        }
+        return render(request, 'blog/edit.html', params)
+
+
+def delete(request, article_id):
+    article = Article.objects.get(id=article_id)
+    if (request.method == 'POST'):
+        article.delete()
+        return redirect('index')
+    else:
+        params = {
+            'article': article,
+        }
+        return render(request, 'blog/delete.html', params)
